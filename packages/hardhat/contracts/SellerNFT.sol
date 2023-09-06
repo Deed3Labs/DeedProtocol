e4e81bb8d947819b61101422a65d85483cfae295
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2023-09-06 14:08:53 -0700

    Update SellerNFT.sol

--- packages/hardhat/contracts/SellerNFT.sol
+++ packages/hardhat/contracts/SellerNFT.sol
@@ -5,24 +5,29 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SellerNFT is ERC721URIStorage, Ownable {
+// EIP-2981 Interface for royalties
+interface IERC2981 {
+    function royaltyInfo(uint256 tokenId, uint256 salePrice) external view returns (address receiver, uint256 royaltyAmount);
+}
+
+contract SellerNFT is ERC721URIStorage, Ownable, IERC2981 {
     using SafeMath for uint256;
 
-    // Mapping of NFT to its total payment received
-    mapping(uint256 => uint256) public totalPaid;
+    address public platformWallet;  // Wallet address to receive platform fees
+    uint256 public constant PLATFORM_FEE = 500;  // 5% in basis points
 
-    // Mapping of NFT to its default status
+    mapping(uint256 => uint256) public totalPaid;
     mapping(uint256 => bool) public isInDefault;
 
-    // Events
     event UpdatedTotalPaid(uint256 indexed tokenId, uint256 newTotal);
     event UpdatedDefaultStatus(uint256 indexed tokenId, bool isInDefault);
 
-    constructor(string memory _name, string memory _symbol)
+    constructor(string memory _name, string memory _symbol, address _platformWallet)
         ERC721(_name, _symbol)
-    {}
+    {
+        platformWallet = _platformWallet;
+    }
 
-    // Function to mint new NFTs
     function mintNFT(address to, uint256 tokenId, string memory tokenURI)
         public onlyOwner
     {
@@ -30,22 +35,18 @@ contract SellerNFT is ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, tokenURI);
     }
 
-    // Function to update total payment received for a token
     function updateTotalPaid(uint256 tokenId, uint256 amountPaid) public onlyOwner {
         require(_exists(tokenId), "Token ID does not exist");
         totalPaid[tokenId] = totalPaid[tokenId].add(amountPaid);
         emit UpdatedTotalPaid(tokenId, totalPaid[tokenId]);
     }
 
-    // Function to update default status
     function updateDefaultStatus(uint256 tokenId, bool _isInDefault) public onlyOwner {
         require(_exists(tokenId), "Token ID does not exist");
         isInDefault[tokenId] = _isInDefault;
         emit UpdatedDefaultStatus(tokenId, _isInDefault);
     }
-}
 
-    // Override function to handle state change on transfer
     function _beforeTokenTransfer(
         address from,
         address to,
@@ -53,18 +54,21 @@ contract SellerNFT is ERC721URIStorage, Ownable {
     ) internal override {
         super._beforeTokenTransfer(from, to, tokenId);
 
-        // Additional logic to handle state changes and logging
         if (from != address(0)) {
-            // Logic to remove state when token is transferred from a valid address
             totalPaid[tokenId] = 0;
             isInDefault[tokenId] = false;
         }
         
         if (to != address(0)) {
-            // Logic to initiate state when token is transferred to a new address
-            // Assuming initial state is zero payment and not in default
             totalPaid[tokenId] = 0;
             isInDefault[tokenId] = false;
         }
+    }
+
+    // EIP-2981 implementation
+    function royaltyInfo(uint256, uint256 salePrice) external view override returns (address receiver, uint256 royaltyAmount) {
+        royaltyAmount = salePrice * PLATFORM_FEE / 10000;
+        receiver = platformWallet;
+        return (receiver, royaltyAmount);
     }
 }
