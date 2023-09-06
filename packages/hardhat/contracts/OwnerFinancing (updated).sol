905d7f60d72120d706b911535fac32d81ea3a490
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2023-09-06 13:58:11 -0700

    Update OwnerFinancing (updated).sol

--- packages/hardhat/contracts/OwnerFinancing (updated).sol
+++ packages/hardhat/contracts/OwnerFinancing (updated).sol
@@ -27,21 +27,25 @@ contract OwnerFinancing {
     SellerNFT private sellerNFTContract;
     BuyerNFT private buyerNFTContract;
     DeedNFT private deedNFTContract;
+    address payable public platformWallet;
 
     mapping(uint256 => FinancingDetails) public financingDetails;
     uint256 public nextFinancingId;
+    uint256 public platformFeeRate = 500; // 5% fee in basis points
 
     uint256 constant MAX_INTEREST_RATE = 3000; // 30% in basis points
-    uint256 constant MAX_ADJUSTABLE_RATE_INCREASE = 1500; // 50% increase in basis points
+    uint256 constant MAX_ADJUSTABLE_RATE_INCREASE = 1500; // 15% increase in basis points
 
     event FinancingCreated(uint256 financingId);
     event PaymentMade(uint256 financingId, address from, uint256 amount);
     event FinancingCompleted(uint256 financingId);
+    event InterestRateChanged(uint256 financingId, uint256 oldRate, uint256 newRate);
 
-    constructor(address _sellerNFTAddress, address _buyerNFTAddress, address _deedNFTAddress) {
+    constructor(address _sellerNFTAddress, address _buyerNFTAddress, address _deedNFTAddress, address payable _platformWallet) {
         sellerNFTContract = SellerNFT(_sellerNFTAddress);
         buyerNFTContract = BuyerNFT(_buyerNFTAddress);
         deedNFTContract = DeedNFT(_deedNFTAddress);
+        platformWallet = _platformWallet;
     }
 
     function createFinancing(address buyer, uint256 price, uint256 termLength, uint256 interestRate, uint256 downPayment, InterestType interestType, uint256 nextAdjustableRateIncrease) public returns (uint256) {
@@ -67,26 +71,31 @@ contract OwnerFinancing {
         return nextFinancingId - 1;
     }
 
-    function acceptTerms(uint256 financingId, uint256 _downPayment) public {
+    function acceptTerms(uint256 financingId, uint256 _downPayment) public payable {
         FinancingDetails storage fd = financingDetails[financingId];
         
         require(msg.sender == fd.buyer, "Only the buyer can accept terms.");
         require(!fd.isTermsAccepted, "Terms are already accepted.");
         require(_downPayment >= fd.downPayment, "Down payment is less than the required amount.");
+        require(msg.value >= _downPayment, "Sent funds are less than the down payment.");
+
+        uint256 platformFee = (_downPayment * PLATFORM_FEE_RATE) / 10000; // Calculate 5% platform fee
+        uint256 toSeller = _downPayment - platformFee;
         
         fd.isTermsAccepted = true;
-        fd.downPayment = _downPayment; // Update downpayment if it's greater
+        fd.downPayment = toSeller; // Update downpayment if it's greater
 
         // Initialize Financing
         fd.isActive = true;
-        fd.totalPaymentsMade = _downPayment;
+        fd.totalPaymentsMade = toSeller; // Exclude platform fee
 
         // Mint NFTs
         sellerNFTContract.mintSellerNFT(fd.seller, financingId);
         buyerNFTContract.mintBuyerNFT(fd.buyer, financingId);
         
-        // Transfer down payment to seller
-        payable(fd.seller).transfer(_downPayment);
+        // Transfer down payment to seller and platform
+        payable(fd.seller).transfer(toSeller);
+        platformWallet.transfer(platformFee);
     }
 
     function makePayment(uint256 financingId) public payable {
@@ -96,14 +105,17 @@ contract OwnerFinancing {
         require(msg.sender == fd.buyer, "Only the buyer can make payments.");
 
         uint256 monthlyPayment = calculateMonthlyPayment(fd.price, fd.termLength, fd.interestRate, fd.downPayment);
+        uint256 platformFee = (monthlyPayment * PLATFORM_FEE_RATE) / 10000; // 5% platform fee
+        uint256 toSeller = monthlyPayment - platformFee;
 
         require(msg.value >= monthlyPayment, "Insufficient payment.");
 
-        // Transfer to the seller
-        payable(fd.seller).transfer(monthlyPayment);
+        // Transfer to the seller and platform
+        payable(fd.seller).transfer(toSeller);
+        platformWallet.transfer(platformFee);
 
         // Update the state
-        fd.totalPaymentsMade += monthlyPayment;
+        fd.totalPaymentsMade += toSeller;
         
         emit PaymentMade(financingId, msg.sender, monthlyPayment);
 
@@ -121,48 +133,42 @@ contract OwnerFinancing {
 
     function completeFinancing(uint256 financingId) private {
         FinancingDetails storage fd = financingDetails[financingId];
-
+        
         require(fd.isActive, "Financing is not active.");
 
         // Transfer the deed to the buyer
         deedNFTContract.transferFrom(fd.seller, fd.buyer, financingId);
         
         fd.isActive = false;
-
+        
         emit FinancingCompleted(financingId);
     }
-    
-    event InterestRateChanged(uint256 financingId, uint256 oldRate, uint256 newRate);
 
     // Option to change interest rate (only by the seller)
     function adjustInterestRate(uint256 financingId, uint256 newInterestRate) public {
         FinancingDetails storage fd = financingDetails[financingId];
-
+        
         require(fd.isActive, "Financing is not active.");
         require(msg.sender == fd.seller, "Only the seller can adjust the interest rate.");
-        require(newInterestRate <= 3000, "Max interest rate is 30%"); // Basis points
+        require(newInterestRate <= MAX_INTEREST_RATE, "Max interest rate is 30%"); // Basis points
         require(newInterestRate > fd.interestRate, "New rate must be greater than the current rate.");
 
-        uint256 maxAllowedAdjustment = (fd.interestRate * 150) / 100;  // 50% max adjustment
+        uint256 maxAllowedAdjustment = (fd.interestRate * MAX_ADJUSTABLE_RATE_INCREASE) / 10000;
         require(newInterestRate <= maxAllowedAdjustment, "Adjustment exceeds max allowed.");
 
         uint256 oldRate = fd.interestRate;
         fd.interestRate = newInterestRate;
-
+        
         emit InterestRateChanged(financingId, oldRate, newInterestRate);
     }
 
-    // Function for the buyer to query the current terms
     function getCurrentTerms(uint256 financingId) public view returns (uint256 price, uint256 termLength, uint256 interestRate, uint256 downPayment) {
         FinancingDetails storage fd = financingDetails[financingId];
-        
         return (fd.price, fd.termLength, fd.interestRate, fd.downPayment);
     }
 
-    // Function for both parties to check the current payment state
     function getCurrentPaymentState(uint256 financingId) public view returns (uint256 totalPaymentsMade, bool isActive) {
         FinancingDetails storage fd = financingDetails[financingId];
-        
         return (fd.totalPaymentsMade, fd.isActive);
     }
 }
