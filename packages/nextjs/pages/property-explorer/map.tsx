658199132dd78bb0d94f34af4d1b67c18d434411
Corantin <corantin.noll@gmail.com>
2023-09-22 19:13:15 -0400

    Remove  token

--- packages/nextjs/pages/property-explorer/map.tsx
+++ packages/nextjs/pages/property-explorer/map.tsx
@@ -1,8 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const token = process.env.NEXT_MAPBOX_TOKEN;
-
 const Map = () => {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: 500, width: "100%" }}>
