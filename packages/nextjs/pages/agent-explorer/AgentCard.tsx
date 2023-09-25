302438ce7284b1c881c5db3174a44ea787df3661
Corantin <corantin.noll@gmail.com>
2023-09-25 15:13:33 -0400

    Check if underined

--- packages/nextjs/pages/agent-explorer/AgentCard.tsx
+++ packages/nextjs/pages/agent-explorer/AgentCard.tsx
@@ -8,46 +8,50 @@ type Props = {
 
 export default function PropertyCard({ agent }: Props) {
   return (
-    <figure
-      className="card bg-base-100 shadow-xl p-2 bg-secondary border border-white border-opacity-10 font-['Montserrat'] w-[500px]"
-      key={agent.id}
-    >
-      <div
-        className="flex flex-col justify-end"
-        style={{
-          backgroundImage: `url(${agent.cover})`,
-          height: "200px",
-          width: "480px",
-        }}
-      >
-        <div className="m-2 w-16">
-          <Image src={agent.profile} alt="Picture" height={128} width={127} />
-        </div>
-      </div>
-      <div className="flex flex-row m-2 justify-between items-center">
-        <div className="m-4">
-          <div className="flex flex-row">
-            <span className="text-2xl font-bold mr-1">{agent.name}</span>
-            <CheckBadgeIcon className="w-6" style={{ color: "#FEDA03" }} />
-          </div>
-          <span className="flex flex-h w-fit gap-2">
-            <span className="text-secondary-content">Username.eth</span>
-          </span>
-        </div>
-        <button type="button" className="btn btn-outline btn-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
+    <>
+      {agent && (
+        <figure
+          className="card bg-base-100 shadow-xl p-2 border border-white border-opacity-10 font-['Montserrat'] w-[500px]"
+          key={agent.id}
+        >
+          <div
+            className="flex flex-col justify-end"
+            style={{
+              backgroundImage: `url(${agent.cover})`,
+              height: "200px",
+              width: "480px",
+            }}
           >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-          Follow
-        </button>
-      </div>
-    </figure>
+            <div className="m-2 w-16">
+              <Image src={agent.profile} alt="Picture" height={128} width={127} />
+            </div>
+          </div>
+          <div className="flex flex-row m-2 justify-between items-center">
+            <div className="m-4">
+              <div className="flex flex-row">
+                <span className="text-2xl font-bold mr-1">{agent.name}</span>
+                <CheckBadgeIcon className="w-6" style={{ color: "#FEDA03" }} />
+              </div>
+              <span className="flex flex-h w-fit gap-2">
+                <span className="text-secondary-content">Username.eth</span>
+              </span>
+            </div>
+            <button type="button" className="btn btn-outline btn-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+              Follow
+            </button>
+          </div>
+        </figure>
+      )}
+    </>
   );
 }
--- packages/nextjs/pages/property-explorer/PropertyCard.tsx
+++ packages/nextjs/pages/property-explorer/PropertyCard.tsx
@@ -8,29 +8,33 @@ type Props = {
 
 export default function PropertyCard({ property }: Props) {
   return (
-    <figure className="card bg-base-100 shadow-xl p-2 border border-white border-opacity-10 font-['Montserrat']">
-      <Image src={property.photos[0]} alt="Picture" height={400} width={350} />
-      <div className="m-4">
-        <span className="flex flex-h w-fit gap-2">
-          <span className="text-secondary-content">Username.eth</span>
-          <CheckBadgeIcon className="w-4" />
-        </span>
-        <h2 className="text-2xl font-bold">{property.address}</h2>
-      </div>
-      <div className="bg-neutral p-4 flex flex-row justify-evenly border-solid border-2 border-opacity-25">
-        <div className="flex flex-col">
-          <span className="text-secondary-content">PRICE</span>
-          <span>{property.price.toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
-        </div>
-        <div className="flex flex-col">
-          <div className="text-secondary-content">TYPE</div>
-          <div>{property.type}</div>
-        </div>
-        <div className="flex flex-col">
-          <div className="text-secondary-content">ZONING</div>
-          <div>Residential</div>
-        </div>
-      </div>
-    </figure>
+    <>
+      {property && (
+        <figure className="card bg-base-100 shadow-xl p-2 border border-white border-opacity-10 font-['Montserrat']">
+          <Image src={property.photos[0]} alt="Picture" height={400} width={350} />
+          <div className="m-4">
+            <span className="flex flex-h w-fit gap-2">
+              <span className="text-secondary-content">Username.eth</span>
+              <CheckBadgeIcon className="w-4" />
+            </span>
+            <h2 className="text-2xl font-bold">{property.address}</h2>
+          </div>
+          <div className="bg-neutral p-4 flex flex-row justify-evenly border-solid border-2 border-opacity-25">
+            <div className="flex flex-col">
+              <span className="text-secondary-content">PRICE</span>
+              <span>{property.price.toLocaleString("en-US", { style: "currency", currency: "USD" })}</span>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-secondary-content">TYPE</div>
+              <div>{property.type}</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-secondary-content">ZONING</div>
+              <div>Residential</div>
+            </div>
+          </div>
+        </figure>
+      )}
+    </>
   );
 }
