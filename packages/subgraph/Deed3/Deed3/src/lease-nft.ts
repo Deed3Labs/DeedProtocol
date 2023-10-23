5da6415fa2b95d70fb77166a56fb0d8c248c1bd2
Corantin <corantin.noll@gmail.com>
2023-10-23 00:43:31 -0400

    Auto addes subgraph

---
+++ packages/subgraph/Deed3/Deed3/src/lease-nft.ts
@@ -0,0 +1,18 @@
+import { OwnershipTransferred as OwnershipTransferredEvent } from "../generated/LeaseNFT/LeaseNFT"
+import { OwnershipTransferred } from "../generated/schema"
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
