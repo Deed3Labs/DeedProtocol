93a05f3d9cf01c9754d9579311a46b1ff3d546be
Corantin <corantin.noll@gmail.com>
2023-12-12 10:54:35 -0500

    Update next.config.js

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -19,4 +17,4 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
