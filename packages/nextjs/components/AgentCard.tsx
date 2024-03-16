8b4cde9177f4a8904cc17e2b8cc9eb31491c1433
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-16 02:06:08 -0600

    Update AgentCard.tsx

--- packages/nextjs/components/AgentCard.tsx
+++ packages/nextjs/components/AgentCard.tsx
@@ -18,25 +18,22 @@ const AgentCard = ({ agent }: Props) => {
   }, [agent?.followers]);
 
   return (
-    <div className="relative w-80 h-60 p-2 bg-stone-950 border border-white border-opacity-10 flex flex-col justify-end items-start">
-      {/* Content container for name, followers, and follow button */}
-      <div className="px-4 pt-4 flex-1 z-10">
-        <div className="flex items-center gap-1">
-          <span className="text-white text-xl font-bold">{agent.name}</span>
+    <div className="relative w-80 h-60 p-2 bg-stone-950 border border-white border-opacity-10 flex flex-col justify-between">
+      <div className="absolute top-2 left-2">
+        <Image src={agent.profile} alt="Agent Profile" width={56} height={56} layout="fixed" className="" />
+      </div>
+      <div className="pl-20 pt-2">
+        <div className="flex items-center gap-2">
+          <span className="text-xl font-bold">{agent.name}</span>
           <CheckBadgeIcon className="w-5 h-5 text-yellow-400" />
         </div>
-        <div className="text-white text-opacity-60">{followers} Followers</div>
+        <div className="text-secondary-content">{followers} Followers</div>
       </div>
-
-      {/* Follow Button */}
       <Link href={`/agent/${agent.id}`} passHref>
-        <a className="btn btn-neutral btn-xs mb-2 mr-2 self-end z-10">FOLLOW</a>
+        <a className="self-end btn btn-neutral btn-xs mb-2 mr-2">
+          FOLLOW
+        </a>
       </Link>
-
-      {/* Absolute positioned Profile Image at the bottom left */}
-      <div className="absolute bottom-2 left-2 w-14 h-14">
-        <Image src={agent.profile} alt="Agent Profile" layout="fill" objectFit="cover" className="bg-neutral-900" />
-      </div>
     </div>
   );
 };
