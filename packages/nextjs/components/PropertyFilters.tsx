58b9e349a80f3c646e5d0ad3d777afdc82ba5131
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-16 11:55:05 -0600

    Update PropertyFilters.tsx

--- packages/nextjs/components/PropertyFilters.tsx
+++ packages/nextjs/components/PropertyFilters.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useMemo, useState } from "react";
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
-import ExplorerLinks from "./ExplorerLinks";
-import { MapIcon } from "@heroicons/react/24/outline";
-import { MapIcon as MapIconSolid } from "@heroicons/react/24/solid";
-import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
-import { PropertyTypeOptions } from "~~/constants";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid"; // Adjustments icon
+import { MapIcon, MapIconSolid } from "@heroicons/react/24/solid"; // Map icons for toggling
 import useDebouncer from "~~/hooks/useDebouncer";
 import { useKeyboardShortcut } from "~~/hooks/useKeyboardShortcut";
+import { PropertyTypeOptions } from "~~/constants";
 import { PropertyType } from "~~/models/deed-info.model";
 import { PropertiesFilterModel } from "~~/models/properties-filter.model";
 import { ListingType, PropertyModel } from "~~/models/property.model";
@@ -28,6 +26,27 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
 
   const debouncedSearch = useDebouncer(search, 500);
 
+  useEffect(() => {
+    if (debouncedSearch) {
+      applyFilter({ search: debouncedSearch });
+    }
+  }, [debouncedSearch]);
+
+  useEffect(() => {
+    onFilter(filter);
+  }, [filter, listingType]);
+
+  const applyFilter = (partialFilter: Partial<PropertiesFilterModel>) => {
+    const newFilter = { ...filter, ...partialFilter };
+    setFilter(newFilter);
+    onFilter(newFilter);
+  };
+
+  useKeyboardShortcut(["Enter"], () => {
+    onFilter(filter);
+  });
+
+  // Dynamically importing the Map component with SSR disabled
   const Map = useMemo(
     () =>
       dynamic(() => import("~~/components/Map"), {
@@ -41,96 +60,61 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
     [properties],
   );
 
-  useEffect(() => {
-    if (debouncedSearch) {
-      applyFilter({ search: debouncedSearch });
-    }
-  }, [debouncedSearch]);
-
-  useEffect(() => {
-    onFilter(filter);
-  }, [listingType]);
-
-  const applyFilter = (partialFilter: Partial<PropertiesFilterModel>) => {
-    const newFilter = { ...filter, ...partialFilter };
-    setFilter(newFilter);
-    onFilter(filter);
-  };
-
-  useKeyboardShortcut(["Enter"], () => {
-    onFilter(filter);
-  });
-
   return (
     <div className="Wrapper flex flex-col w-full mb-8">
-      <ExplorerLinks />
-      <div className="filters">
-        <div className="flex flex-wrap justify-evenly items-center gap-8 w-full ">
-          <button className="btn btn-lg btn-outline">
-            <AdjustmentsHorizontalIcon className="w-4" />
-            More filters
-          </button>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <input
-                type="checkbox"
-                className="toggle toggle-primary"
-                onChange={ev => {
-                  return applyFilter({ featured: ev.target.checked });
-                }}
-              />
-              <span className="label-text mx-4">Featured?</span>
-            </label>
-          </div>
-          <div className="flex-grow flex items-center">
-            <input
-              className="input input-lg input-bordered border-1 w-full"
-              placeholder="Enter a city, state, address or ZIP code"
-              onChange={val => setSearch(val.target.value)}
-            />
-          </div>
+      {/* Filters Container */}
+      <div className="filters w-full h-14 justify-start items-center gap-3.5 inline-flex bg-neutral-900 border border-white border-opacity-20">
+        {/* More Filters Button */}
+        <button className="btn flex items-center justify-center gap-2 p-3 text-white" onClick={() => {}}>
+          <AdjustmentsHorizontalIcon className="w-5 h-5" />
+          More filters
+        </button>
+        
+        {/* Featured Toggle */}
+        <div className="form-control flex items-center">
+          <label className="cursor-pointer label flex items-center gap-2">
+            <input type="checkbox" className="toggle toggle-primary" onChange={(ev) => applyFilter({ featured: ev.target.checked })} />
+            <span className="label-text text-white">Featured?</span>
+          </label>
+        </div>
+        
+        {/* Search Input */}
+        <div className="form-control flex-grow">
+          <input
+            className="input input-lg input-bordered w-full bg-neutral-900 text-white placeholder-white::placeholder"
+            placeholder="Enter a city, state, address or ZIP code"
+            onChange={(ev) => setSearch(ev.target.value)}
+          />
+        </div>
+        
+        {/* Property Type Selector */}
+        <div className="form-select">
           <select
-            className="select select-lg select-bordered"
+            className="select select-lg select-bordered bg-neutral-900 text-white"
             value={filter.propertyType}
-            onChange={ev => applyFilter({ propertyType: ev.target.value as PropertyType })}
+            onChange={(ev) => applyFilter({ propertyType: ev.target.value as PropertyType })}
           >
-            <option disabled value={0}>
+            <option disabled value="">
               Property type
             </option>
-            {PropertyTypeOptions.map(option => (
+            {PropertyTypeOptions.map((option) => (
               <option key={option.value} value={option.value}>
                 {option.title}
               </option>
             ))}
           </select>
-          <div className="join">
-            <button className="join-item btn btn-square btn-outline">
-              <svg
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.75 0.242325C7.92155 0.242325 7.25 0.913897 7.25 1.74232V3.99233C7.25 4.82078 7.92155 5.49233 8.75 5.49233H11C11.8285 5.49233 12.5 4.82078 12.5 3.99233V1.74232C12.5 0.913897 11.8285 0.242325 11 0.242325H8.75ZM8.75 6.99232C7.92155 6.99232 7.25 7.66387 7.25 8.49232V10.7423C7.25 11.5708 7.92155 12.2423 8.75 12.2423H11C11.8285 12.2423 12.5 11.5708 12.5 10.7423V8.49232C12.5 7.66387 11.8285 6.99232 11 6.99232H8.75ZM0.5 8.49232C0.5 7.66387 1.17157 6.99232 2 6.99232H4.25C5.07845 6.99232 5.75 7.66387 5.75 8.49232V10.7423C5.75 11.5708 5.07845 12.2423 4.25 12.2423H2C1.17157 12.2423 0.5 11.5708 0.5 10.7423V8.49232ZM2 0.242325C1.17157 0.242325 0.5 0.913897 0.5 1.74232V3.99233C0.5 4.82078 1.17157 5.49233 2 5.49233H4.25C5.07845 5.49233 5.75 4.82078 5.75 3.99233V1.74232C5.75 0.913897 5.07845 0.242325 4.25 0.242325H2Z"
-                  fill="white"
-                />
-              </svg>
-            </button>
-            <button
-              className="join-item btn btn-square btn-outline"
-              onClick={() => setMapOpened(!mapOpened)}
-            >
-              {mapOpened ? <MapIconSolid className="w-4" /> : <MapIcon className="w-4" />}
-            </button>
-          </div>
         </div>
+        
+        {/* Map Toggle */}
+        <button className="btn btn-square btn-outline" onClick={() => setMapOpened(!mapOpened)}>
+          {mapOpened ? <MapIconSolid className="w-6 h-6 text-white" /> : <MapIcon className="w-6 h-6 text-white" />}
+        </button>
       </div>
+
+      {/* Map Component */}
       {mapOpened && <Map markers={properties} />}
     </div>
   );
 };
+
 export default PropertyFilters;
