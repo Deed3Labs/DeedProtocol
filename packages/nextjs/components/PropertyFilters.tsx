f2747837a4fb74eb06d6f1f2109e3ac74aa533dd
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-16 13:44:21 -0600

    Update PropertyFilters.tsx

--- packages/nextjs/components/PropertyFilters.tsx
+++ packages/nextjs/components/PropertyFilters.tsx
@@ -2,9 +2,8 @@ import React, { useEffect, useMemo, useState } from "react";
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 import ExplorerLinks from "./ExplorerLinks";
-import { MapIcon } from "@heroicons/react/24/outline";
-import { MapIcon as MapIconSolid } from "@heroicons/react/24/solid";
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/24/outline"; // Assuming this is used for the dropdown
 import { PropertyTypeOptions } from "~~/constants";
 import useDebouncer from "~~/hooks/useDebouncer";
 import { useKeyboardShortcut } from "~~/hooks/useKeyboardShortcut";
@@ -29,16 +28,10 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
   const debouncedSearch = useDebouncer(search, 500);
 
   const Map = useMemo(
-    () =>
-      dynamic(() => import("~~/components/Map"), {
-        loading: () => (
-          <div className="w-full flex flex-row justify-center">
-            <span className="loading loading-bars loading-lg"></span>
-          </div>
-        ),
-        ssr: false,
-      }),
-    [properties],
+    () => dynamic(() => import("~~/components/Map"), {
+      loading: () => <div className="w-full flex justify-center"><span>Loading...</span></div>,
+      ssr: false,
+    }), [properties]
   );
 
   useEffect(() => {
@@ -49,83 +42,58 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
 
   useEffect(() => {
     onFilter(filter);
-  }, [listingType]);
+  }, [filter]);
 
   const applyFilter = (partialFilter: Partial<PropertiesFilterModel>) => {
     const newFilter = { ...filter, ...partialFilter };
     setFilter(newFilter);
-    onFilter(filter);
+    onFilter(newFilter);
   };
 
-  useKeyboardShortcut(["Enter"], () => {
-    onFilter(filter);
-  });
+  useKeyboardShortcut(["Enter"], () => onFilter(filter));
 
   return (
-    <div className="Wrapper flex flex-col w-full mb-8">
-      <ExplorerLinks />
-      <div className="filters">
-        <div className="flex flex-wrap justify-evenly items-center gap-8 w-full ">
-          <button className="btn btn-lg btn-outline">
-            <AdjustmentsHorizontalIcon className="w-4" />
-            More filters
+    <div className="flex flex-col items-center justify-center w-full mb-8 p-7 bg-stone-950">
+      <div className="flex flex-col items-start justify-end self-stretch h-36 gap-7">
+        <div className="flex items-center justify-start gap-6 pr-24">
+          <div className="text-5xl font-black text-white font-[Coolvetica]">ALL PROPERTIES</div>
+          <div className="text-5xl font-black text-white text-opacity-30 font-[Coolvetica]">FOR SALE</div>
+          <div className="text-5xl font-black text-white text-opacity-30 font-[Coolvetica]">FOR LEASE</div>
+          <div className="text-5xl font-black text-white text-opacity-30 font-[Coolvetica]">AGENT DIRECTORY</div>
+        </div>
+        <div className="flex items-center justify-start gap-3.5 self-stretch">
+          {/* Filters section based on Figma design */}
+          <button className="flex items-center justify-center gap-1.5 bg-neutral-900 border border-white border-opacity-20 p-1.5 w-24">
+            <AdjustmentsHorizontalIcon className="w-5 h-5" />
+            <span className="text-xs font-semibold text-white">More Filters</span>
           </button>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <input
-                type="checkbox"
-                className="toggle toggle-primary"
-                onChange={ev => {
-                  return applyFilter({ featured: ev.target.checked });
-                }}
-              />
-              <span className="label-text mx-4">Featured?</span>
+          {/* Assuming toggle switch for 'Featured?' is a styled checkbox for simplicity */}
+          <div className="flex items-center gap-2">
+            <label className="switch">
+              <input type="checkbox" onChange={(ev) => applyFilter({ featured: ev.target.checked })} />
+              <span className="slider"></span>
             </label>
+            <span className="text-xs font-medium text-white text-opacity-60">Featured?</span>
           </div>
-          <div className="flex-grow flex items-center">
+          <div className="flex items-center gap-2 p-1.5 bg-neutral-900 border border-white border-opacity-20 grow">
             <input
-              className="input input-lg input-bordered border-1 w-full"
+              className="input w-full bg-transparent text-white placeholder-white::placeholder"
               placeholder="Enter a city, state, address or ZIP code"
-              onChange={val => setSearch(val.target.value)}
+              onChange={(val) => setSearch(val.target.value)}
             />
           </div>
-          <select
-            className="select select-lg select-bordered"
-            value={filter.propertyType}
-            onChange={ev => applyFilter({ propertyType: ev.target.value as PropertyType })}
-          >
-            <option disabled value={0}>
-              Property type
-            </option>
-            {PropertyTypeOptions.map(option => (
-              <option key={option.value} value={option.value}>
-                {option.title}
-              </option>
-            ))}
-          </select>
-          <div className="join">
-            <button className="join-item btn btn-square btn-outline">
-              <svg
-                width="13"
-                height="13"
-                viewBox="0 0 13 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8.75 0.242325C7.92155 0.242325 7.25 0.913897 7.25 1.74232V3.99233C7.25 4.82078 7.92155 5.49233 8.75 5.49233H11C11.8285 5.49233 12.5 4.82078 12.5 3.99233V1.74232C12.5 0.913897 11.8285 0.242325 11 0.242325H8.75ZM8.75 6.99232C7.92155 6.99232 7.25 7.66387 7.25 8.49232V10.7423C7.25 11.5708 7.92155 12.2423 8.75 12.2423H11C11.8285 12.2423 12.5 11.5708 12.5 10.7423V8.49232C12.5 7.66387 11.8285 6.99232 11 6.99232H8.75ZM0.5 8.49232C0.5 7.66387 1.17157 6.99232 2 6.99232H4.25C5.07845 6.99232 5.75 7.66387 5.75 8.49232V10.7423C5.75 11.5708 5.07845 12.2423 4.25 12.2423H2C1.17157 12.2423 0.5 11.5708 0.5 10.7423V8.49232ZM2 0.242325C1.17157 0.242325 0.5 0.913897 0.5 1.74232V3.99233C0.5 4.82078 1.17157 5.49233 2 5.49233H4.25C5.07845 5.49233 5.75 4.82078 5.75 3.99233V1.74232C5.75 0.913897 5.07845 0.242325 4.25 0.242325H2Z"
-                  fill="white"
-                />
-              </svg>
-            </button>
-            <button
-              className="join-item btn btn-square btn-outline"
-              onClick={() => setMapOpened(!mapOpened)}
+          <div className="flex items-center justify-center bg-neutral-900 border border-white border-opacity-20 p-1.5">
+            <select
+              className="bg-transparent text-white"
+              value={filter.propertyType}
+              onChange={(ev) => applyFilter({ propertyType: ev.target.value as PropertyType })}
             >
-              {mapOpened ? <MapIconSolid className="w-4" /> : <MapIcon className="w-4" />}
-            </button>
+              <option disabled value="">Property type</option>
+              {PropertyTypeOptions.map((option) => (
+                <option key={option.value} value={option.value}>{option.title}</option>
+              ))}
+            </select>
+            <ChevronDownIcon className="w-5 h-5 text-white" />
           </div>
         </div>
       </div>
@@ -133,4 +101,5 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
     </div>
   );
 };
+
 export default PropertyFilters;
