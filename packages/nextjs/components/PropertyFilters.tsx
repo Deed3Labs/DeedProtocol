df9b6c513e5084b643a48bd971dfd17bbd87fc1e
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-17 17:47:43 -0600

    Update PropertyFilters.tsx

--- packages/nextjs/components/PropertyFilters.tsx
+++ packages/nextjs/components/PropertyFilters.tsx
@@ -64,9 +64,9 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
   return (
     <div className="Wrapper flex flex-col w-full mb-8">
       <ExplorerLinks />
-      <div className="filters bg-gray-800 text-white p-4 rounded-lg">
+      <div className="filters bg-gray-800 filters-bordered text-white p-4 rounded-lg">
         <div className="flex flex-row flex-wrap justify-between items-center gap-4 w-full">
-          <button className="btn btn-sm flex items-center gap-2 border-gray-700 text-white bg-gray-800 hover:bg-gray-700">
+          <button className="btn btn-sm flex items-center gap-2">
             <AdjustmentsHorizontalIcon className="h-5 w-5" />
             More filters
           </button>
@@ -82,13 +82,13 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
             </label>
           </div>
           <input
-            className="input input-sm input-bordered w-full max-w-xs border-gray-700 bg-gray-800 text-white"
+            className="input input-sm input-bordered w-full max-w-xs"
             placeholder="Enter a city, state, address"
             value={search}
             onChange={e => setSearch(e.target.value)}
           />
           <select
-            className="select select-sm w-full max-w-xs border-gray-700 bg-gray-800 text-white"
+            className="select select-sm select-primary select-bordered w-full max-w-xs"
             value={filter.propertyType}
             onChange={ev => applyFilter({ propertyType: ev.target.value as PropertyType })}
           >
@@ -99,9 +99,28 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
               </option>
             ))}
           </select>
-          <div className="flex space-x-2">
-            <button className="btn btn-square btn-outline border-gray-700 bg-gray-800 hover:bg-gray-700" onClick={() => setMapOpened(!mapOpened)}>
-              {mapOpened ? <MapIconSolid className="w-5 h-5 text-white" /> : <MapIcon className="w-5 h-5 text-white" />}
+          <div className="join">
+            <button className="join-item btn btn-square btn-outline">
+              <svg
+                width="13"
+                height="13"
+                viewBox="0 0 13 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8.75 0.242325C7.92155 0.242325 7.25 0.913897 7.25 1.74232V3.99233C7.25 4.82078 7.92155 5.49233 8.75 5.49233H11C11.8285 5.49233 12.5 4.82078 12.5 3.99233V1.74232C12.5 0.913897 11.8285 0.242325 11 0.242325H8.75ZM8.75 6.99232C7.92155 6.99232 7.25 7.66387 7.25 8.49232V10.7423C7.25 11.5708 7.92155 12.2423 8.75 12.2423H11C11.8285 12.2423 12.5 11.5708 12.5 10.7423V8.49232C12.5 7.66387 11.8285 6.99232 11 6.99232H8.75ZM0.5 8.49232C0.5 7.66387 1.17157 6.99232 2 6.99232H4.25C5.07845 6.99232 5.75 7.66387 5.75 8.49232V10.7423C5.75 11.5708 5.07845 12.2423 4.25 12.2423H2C1.17157 12.2423 0.5 11.5708 0.5 10.7423V8.49232ZM2 0.242325C1.17157 0.242325 0.5 0.913897 0.5 1.74232V3.99233C0.5 4.82078 1.17157 5.49233 2 5.49233H4.25C5.07845 5.49233 5.75 4.82078 5.75 3.99233V1.74232C5.75 0.913897 5.07845 0.242325 4.25 0.242325H2Z"
+                  fill="white"
+                />
+              </svg>
+            </button>
+            <button
+              className="join-item btn btn-square btn-outline"
+              onClick={() => setMapOpened(!mapOpened)}
+            >
+              {mapOpened ? <MapIconSolid className="w-4" /> : <MapIcon className="w-4" />}
             </button>
           </div>
         </div>
@@ -112,4 +131,5 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
 };
 
 export default PropertyFilters;
+
 
