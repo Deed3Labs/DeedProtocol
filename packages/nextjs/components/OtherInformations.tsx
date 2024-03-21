f8021e5e928ecd76f78972552ad8cb689e09b7e3
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-20 19:22:18 -0600

    Update OtherInformations.tsx

--- packages/nextjs/components/OtherInformations.tsx
+++ packages/nextjs/components/OtherInformations.tsx
@@ -11,41 +11,43 @@ interface Props {
   readOnly?: boolean;
 }
 
-const OtherInformations = ({ value, onChange, readOnly }: Props) => {
+const OtherInformation = ({ value, onChange, readOnly }: Props) => {
   const handleChange = (ev: LightChangeEvent<OtherInformationModel>) => {
     const updatedValue = { ...value, [ev.name]: ev.value };
     onChange?.({ name: "otherInformation", value: updatedValue });
   };
+
   return (
     <div className="flex flex-col gap-6 mt-6">
-  <div className="text-4xl font-['Coolvetica'] font-condensed uppercase">
-    3. Other Information
-  </div>
-
-  {/* Wrapper for RadioBoxesInput with conditional styling */}
-  <div className="max-w-4xl mx-auto">
-    <RadioBoxesInput
-      name="wrapper"
-      label="Customize your Wrapper type"
-      description={
-        <>
-          <Link
-            href="https://docs.deedprotocol.org/legal-framework/property-wrappers"
-            target="_blank"
-          >
-            Learn more
-          </Link>
-          &nbsp;about each Wrapper type.
-        </>
-      }
-      options={WrapperOptions(["$225.00", "$450.00"])}
-      value={value?.wrapper}
-      onChange={handleChange}
-      readOnly={readOnly}
-    />
-  </div>
-</div>
+      <div className="text-4xl font-['Coolvetica'] font-condensed uppercase">
+        3. Other Information
+      </div>
+      {/* Container that limits width and changes layout for this specific instance */}
+      <div className="custom-radio-layout">
+        <RadioBoxesInput
+          name="wrapper"
+          label="Customize your Wrapper type"
+          description={
+            <>
+              <Link
+                href="https://docs.deedprotocol.org/legal-framework/property-wrappers"
+                target="_blank"
+              >
+                Learn more
+              </Link>
+              &nbsp;about each Wrapper type.
+            </>
+          }
+          options={WrapperOptions(["$225.00", "$450.00"])}
+          optionsClassName="w-full" // Ensure this class allows for full width
+          value={value?.wrapper}
+          onChange={handleChange}
+          readOnly={readOnly}
+        />
+      </div>
+    </div>
   );
 };
 
-export default OtherInformations;
+export default OtherInformation;
+
