f83a4ece993d9047824d4d654e7b16dae65ef4ba
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-20 20:18:03 -0600

    Update OtherInformations.tsx

--- packages/nextjs/components/OtherInformations.tsx
+++ packages/nextjs/components/OtherInformations.tsx
@@ -11,59 +11,41 @@ interface Props {
   readOnly?: boolean;
 }
 
-const OtherInformation = ({ value, onChange, readOnly }: Props) => {
+const OtherInformations = ({ value, onChange, readOnly }: Props) => {
   const handleChange = (ev: LightChangeEvent<OtherInformationModel>) => {
     const updatedValue = { ...value, [ev.name]: ev.value };
     onChange?.({ name: "otherInformation", value: updatedValue });
   };
-
+  
   return (
     <div className="flex flex-col gap-6 mt-6">
       <div className="text-4xl font-['Coolvetica'] font-condensed uppercase">
         3. Other Information
       </div>
-      <RadioBoxesInput
-        name="wrapper"
-        label="Customize your Wrapper type"
-        description={
-          <>
-            <Link
-              href="https://docs.deedprotocol.org/legal-framework/property-wrappers"
-              target="_blank"
-            >
-              Learn more
-            </Link>
-            &nbsp;about each Wrapper type.
-          </>
-        }
-        options={WrapperOptions(["$225.00", "$450.00"])}
-        value={value?.wrapper}
-        onChange={handleChange}
-        readOnly={readOnly}
-      />
-      {/* The grid setup for the RadioBoxesInput's options should be modified as per the suggestion */}
-      <div className="grid grid-cols-1 gap-4 mt-2">
-        {WrapperOptions(["$225.00", "$450.00"]).map((option, index) => (
-          <div key={index} className="border border-gray-700 p-4 rounded-lg bg-gray-800">
-            <input
-              id={`wrapper_option_${option.value}`}
-              name="wrapper"
-              type="radio"
-              value={option.value}
-              checked={value?.wrapper === option.value}
-              onChange={() => handleChange({ value: option.value, name: "wrapper" })}
-              className="hidden"
-            />
-            <label htmlFor={`wrapper_option_${option.value}`} className="block w-full cursor-pointer">
-              <div className="text-lg font-bold text-white">{option.title}</div>
-              <div className="text-white">{option.subtitle}</div>
-              <div className="text-gray-400">{option.tag}</div>
-            </label>
-          </div>
-        ))}
+      <div className="flex flex-col"> {/* This wrapper applies the flex-col layout */}
+        <RadioBoxesInput
+          name="wrapper"
+          label="Customize your Wrapper type"
+          description={
+            <>
+              <Link
+                href="https://docs.deedprotocol.org/legal-framework/property-wrappers"
+                target="_blank"
+              >
+                Learn more
+              </Link>
+              &nbsp;about each Wrapper type.
+            </>
+          }
+          options={WrapperOptions(["$225.00", "$450.00"])}
+          optionsClassName="w-full"
+          value={value?.wrapper}
+          onChange={handleChange}
+          readOnly={readOnly}
+        />
       </div>
     </div>
   );
 };
 
-export default OtherInformation;
+export default OtherInformations;
