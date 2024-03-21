11e3141da07146ce9d107ef1a628b6d755917c21
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-20 22:26:43 -0600

    Update OwnerInformation.tsx

--- packages/nextjs/components/OwnerInformation.tsx
+++ packages/nextjs/components/OwnerInformation.tsx
@@ -47,7 +47,7 @@ const OwnerInformation = ({ value, onChange, readOnly }: Props) => {
         value={value?.ownerType}
         readOnly={readOnly}
       ></RadioBoxesInput>
-      <div className="flex flex-row flex-wrap gap-3 justify-start w-full h-[180px]">
+      <div className="flex flex-row flex-wrap gap-3 justify-start w-full">
         {value?.ownerType === "legal" && (
           <TextInput
             name="entityName"
