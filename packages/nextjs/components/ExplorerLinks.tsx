6c03056be59404c9781f4c334fb0de1b011bf50f
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-22 01:51:13 -0600

    Update ExplorerLinks.tsx

--- packages/nextjs/components/ExplorerLinks.tsx
+++ packages/nextjs/components/ExplorerLinks.tsx
@@ -6,7 +6,7 @@ const ExplorerLinks = () => {
   const pathname = usePathname();
   const type = searchParams.get("type");
   return (
-    <div className="grid auto-cols-min grid-cols-4 px-1 gap-2 lg:gap-6 text-4xl sm:text-6xl m-8 font-['Coolvetica'] font-extra-condensed font-bold uppercase">
+    <div className="grid grid-flow-col auto-cols-auto px-1 gap-2 lg:gap-6 text-4xl sm:text-6xl m-8 font-['Coolvetica'] font-extra-condensed font-bold uppercase">
       <Link
         className={`${
           (type !== "all" && type) || pathname === "/agent-explorer" ? "opacity-40" : ""
