8fc51f7b70b1e928d9f50f45482184d17d65e081
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-24 22:44:33 -0600

    Update PropertyFilters.tsx

--- packages/nextjs/components/PropertyFilters.tsx
+++ packages/nextjs/components/PropertyFilters.tsx
@@ -72,7 +72,7 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
             value={search}
             onChange={e => setSearch(e.target.value)}
           />
-          <button className="btn btn-md sm:button-lg btn-outline bg-neutral-900 text-sm sm:text-base font-normal capitalize flex flex-grow items-center gap-2 h-full">
+          <button className="btn btn-md sm:button-lg btn-outline bg-neutral-900 text-sm sm:text-base font-normal capitalize flex items-center gap-2 w-auto h-full">
             <AdjustmentsHorizontalIcon className="h-auto w-4" />
             More Filters
           </button>
