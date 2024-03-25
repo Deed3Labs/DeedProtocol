067d6ac0d9f01bbe30ac6d2fbb43229c9fb1d726
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-25 15:51:29 -0600

    Update QuoteDetail.tsx

--- packages/nextjs/components/scaffold-eth/QuoteDetail.tsx
+++ packages/nextjs/components/scaffold-eth/QuoteDetail.tsx
@@ -10,10 +10,10 @@ export const USDollar = new Intl.NumberFormat("en-US", {
 
 const QuoteDetails = ({ price, title, secondary }: Props) => {
   return (
-    <div className="flex flex-col my-2 text-xl">
+    <div className="flex flex-col my-2 text-lg">
       <div className="flex flex-row h-fit items-center mb-0 justify-between">
-        <span className="text-base font-normal">{title}</span>
-        <span className="text-base font-normal">{USDollar.format(price)}</span>
+        <span className="font-normal">{title}</span>
+        <span className="font-normal">{USDollar.format(price)}</span>
       </div>
       {secondary && <span className="text-zinc-400 text-[10px] uppercase tracking-widest">{secondary}</span>}
     </div>
