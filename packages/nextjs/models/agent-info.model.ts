e15596ead86f3b5709a74bf9cce9bd4217f065a3
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-25 11:12:30 -0600

    Create agent-info.model.ts

---
+++ packages/nextjs/models/agent-info.model.ts
@@ -0,0 +1,105 @@
+import { FileFieldKeyLabel, FileModel, FileValidationState } from "./file.model";
+import { Address } from "viem";
+import {
+  EntityTypeOptions,
+  OwnerTypeOptions,
+  PaymentOptions,
+  PropertySubtypeOptions,
+  PropertyTypeOptions,
+  PropertyZoningOptions,
+  StateOptions,
+  WrapperOptions,
+} from "~~/constants";
+import { ValueExtractor } from "~~/utils/extract-values";
+
+export interface OwnerInformationModel {
+  ownerType: ValueExtractor<typeof OwnerTypeOptions>;
+
+  ownerName: string;
+  ownerSuffix?: string;
+
+  // -- Coorporation only --
+  entityName: string;
+  ownerPosition: string;
+  ownerState: ValueExtractor<typeof StateOptions>;
+  ownerEntityType?: ValueExtractor<typeof EntityTypeOptions>;
+
+  ids: FileModel;
+  proofBill?: FileModel;
+  articleIncorporation: FileModel;
+  operatingAgreement?: FileModel;
+  supportingDoc?: FileModel[];
+}
+
+export type PropertyType = ValueExtractor<typeof PropertyTypeOptions>;
+
+export interface PropertyDetailsModel {
+  propertyType: PropertyType;
+  propertySubType?: ValueExtractor<typeof PropertySubtypeOptions>;
+  propertyAddress: string;
+  propertyCity: string;
+  propertyState: ValueExtractor<typeof StateOptions>;
+  propertySize?: string;
+  propertyZoning?: ValueExtractor<typeof PropertyZoningOptions>;
+  propertyBedrooms?: string;
+  propertyBathrooms?: string;
+  propertyBuildYear?: string;
+  propertyHouseType?: string;
+  propertySquareFootage?: string;
+
+  propertyImages?: FileModel[];
+  propertyDeedOrTitle: FileModel;
+  propertyPurchaseContract?: FileModel;
+
+  stateFillings?: FileModel[];
+}
+
+export interface OtherInformationModel {
+  wrapper: ValueExtractor<typeof WrapperOptions>;
+}
+
+export interface PaymentInformationModel {
+  paymentType: ValueExtractor<typeof PaymentOptions>;
+  promoCode?: string;
+  receipt?: string;
+
+  // -- Fiat only --
+  cardNumber?: string;
+  cardExpiry?: string;
+  cardCVV?: string;
+  cardholderName?: string;
+  suffix?: string;
+  billingAddress?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+
+  // -- Crypto only --
+  stableCoin?: Address;
+}
+
+export interface DeedInfoModel {
+  id?: string;
+  owner?: Address;
+  isValidated?: boolean;
+  timestamp?: number;
+
+  // 1. Owner Information
+  ownerInformation: OwnerInformationModel;
+
+  // 2. Property Details
+  propertyDetails: PropertyDetailsModel;
+
+  // 3. Other Information
+  otherInformation: OtherInformationModel;
+
+  //4. Payment Information
+  paymentInformation: PaymentInformationModel;
+
+  process?: FileModel[];
+  agreement?: FileModel[];
+  documentNotorization?: FileModel[];
+
+  validations?: [string, FileValidationState][];
+  signatureTx?: string;
+}
