6e14990024f436db13ac5a39c835e501cab1988c
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-24 23:38:12 -0600

    Update PropertyFilters.tsx

--- packages/nextjs/components/PropertyFilters.tsx
+++ packages/nextjs/components/PropertyFilters.tsx
@@ -65,7 +65,7 @@ const PropertyFilters = ({ properties, onFilter }: Props) => {
     <div className="Wrapper flex flex-col space-y-[-20px] sm:space-y-[-16px] w-full mb-8">
       <ExplorerLinks />
       <div className="filters">
-        <div className="flex flex-row flex-nowrap sm:flex-nowrap justify-start items-center gap-2 md:gap-4 w-full">
+        <div className="flex flex-row flex-wrap sm:flex-nowrap justify-start items-center gap-2 md:gap-4 w-full">
           <input
             className="input input-md sm:input-lg input-bordered w-full sm:flex-grow"
             placeholder="Search by City, State, or Zip code"
