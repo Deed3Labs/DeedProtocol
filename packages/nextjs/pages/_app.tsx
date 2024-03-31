09cb15260f5ab4c22cb66743a74b1eef5b2fd0ad
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-30 19:04:27 -0600

    Update _app.tsx

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -29,6 +29,10 @@ const ScaffoldEthApp = (props: AppProps) => {
 
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+        {/* Other meta tags */}
+      </Head>
       <NextNProgress
         options={{
           showSpinner: false,
