1e0227ba77e3048b1ba8cbb6843ac05fcc39a415
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-03-31 14:44:12 -0600

    Update PropertyCard.tsx

--- packages/nextjs/components/PropertyCard.tsx
+++ packages/nextjs/components/PropertyCard.tsx
@@ -24,19 +24,19 @@ const PropertyCard = ({ property }: Props) => {
                 <div class="pr-16 justify-start items-center inline-flex">
                   <div class="justify-start items-center gap-1 flex">
                     <div class="h-5 flex-col justify-center items-start inline-flex">
-                      <div class="text-white text-opacity-60 text-[9px] sm:text-xs font-medium leading-none tracking-wider">USERNAME.ETH</div>
+                      <div class="text-white text-opacity-60 text-[9px] sm:text-[10px] font-normal leading-none tracking-wider">USERNAME.ETH</div>
                     </div>
                   </div>
                 </div>
                 <div class="self-stretch justify-start items-center w-full">
-                  <div class="text-white text-base font-normal w-full leading-snug">{property.address}</div>
+                  <div class="text-white text-sm sm:text-base font-normal w-full leading-snug">{property.address}</div>
                 </div>
               </div>
-              <div class="self-stretch h-16 px-3 pt-2.5 pb-3 bg-neutral-900 border border-white border-opacity-10 flex-col justify-center items-start flex">
+              <div class="self-stretch h-16 px-2.5 pt-2.5 pb-2.5 bg-[#0e0e0e] border border-white border-opacity-10 flex-col justify-center items-start flex">
                 <div class="self-stretch justify-between items-start gap-2.5 inline-flex">
                   <div class="flex-col justify-start items-start inline-flex">
                     <div class="self-stretch h-4 flex-col justify-start items-start flex">
-                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68w] font-medium leading-none tracking-wider">PRICE</div>
+                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68vw] font-normal leading-none tracking-wider">PRICE</div>
                     </div>
                     <div class="self-stretch justify-start items-start gap-0.5 inline-flex">
                       <div class="text-white text-[2vw] sm:text-[0.72vw] font-medium uppercase leading-none">
@@ -50,7 +50,7 @@ const PropertyCard = ({ property }: Props) => {
                   </div>
                   <div class="flex-col justify-start items-start inline-flex">
                     <div class="self-stretch h-4 flex-col justify-start items-start flex">
-                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68vw] font-medium leading-none tracking-wider">TYPE</div>
+                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68vw] font-normal leading-none tracking-wider">TYPE</div>
                     </div>
                     <div class="self-stretch justify-start items-start inline-flex">
                       <div class="text-white text-[1.8vw] sm:text-[0.68vw] font-medium uppercase leading-none">{property.type}</div>
@@ -58,7 +58,7 @@ const PropertyCard = ({ property }: Props) => {
                   </div>
                   <div class="flex-col justify-start items-start inline-flex">
                     <div class="self-stretch h-4 flex-col justify-start items-start flex">
-                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68vw] font-medium leading-none tracking-wider">ZONING</div>
+                      <div class="text-white text-opacity-60 text-[1.8vw] sm:text-[0.68vw] font-normal leading-none tracking-wider">ZONING</div>
                     </div>
                     <div class="justify-start items-start inline-flex">
                       <div class="text-white text-[1.8vw] sm:text-[0.68vw] font-medium uppercase leading-none">Residential</div>
