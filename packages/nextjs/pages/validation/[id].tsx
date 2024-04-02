8e41db024fb64efb28135d4425d48b9eb3b60f79
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-02 12:46:47 -0600

    Update [id].tsx

--- packages/nextjs/pages/validation/[id].tsx
+++ packages/nextjs/pages/validation/[id].tsx
@@ -162,30 +162,26 @@ const Page = ({ router }: WithRouterProps) => {
                 />
                 </svg>
               </button>
+              <div className="flex flex-row gap-4">
+                <div className="join join-horizontal border border-white border-opacity-10 p-2">
+                  <div
+                    className="join-item p-2 text-[14px] text-[#626262] cursor-default"
+                    title="Coming Soon"
+                  >
+                    Property Overview
+                  </div>
+                  <div className="join-item p-2 text-[14px] bg-base-300 cursor-pointer">
+                    Validation History
+                  </div>
+                </div>
+  
               <div className="dropdown dropdown-end">
-                <button
-                  tabIndex={0}
-                  className="btn m-1 border-white border-opacity-10 btn-square rounded-lg"
-               </div>
-                    <div className="join join-horizontal border border-white border-opacity-10 p-2">
-                      <div
-                        className="join-item p-2 text-[14px] text-[#626262] cursor-default"
-                        title="Comming Soon"
-                      >
-                        Property Overview
-                      </div>
-                      <div className="join-item p-2 text-[14px] bg-base-300 cursor-pointer">
-                        Validation History
-                      </div>
-                    </div>
-                    <div className="dropdown dropdown-end">
-                      <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn m-1 border-white border-opacity-10 btn-square rounded-lg"   
-                >
-                  <EllipsisHorizontalIcon className="h-6" />
-                </button>
+                  <button
+                    tabIndex={0}
+                    className="btn m-1 border-white border-opacity-10 btn-square rounded-lg"
+                  >
+                    <EllipsisHorizontalIcon className="h-6" />
+                  </button>
                 <ul
                   tabIndex={0}
                   className="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-52"
