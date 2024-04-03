f320bd93480a2c926721e7be19b0e2922e0673fc
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2024-04-02 20:25:28 -0700

    Revert "Update ValidationPropertyOverview.tsx"
    This reverts commit 7a07f3f9d7629dee59c12b92c07384f8fb41050f.

--- packages/nextjs/components/ValidationPropertyOverview.tsx
+++ packages/nextjs/components/ValidationPropertyOverview.tsx
@@ -52,93 +52,103 @@ const PropertyOverview = ({ deedData, isOwner, isValidator, refresh }: Props) =>
   };
 
   return (
-    <div className="flex flex-col gap-6 mt-4">
-      <div className="flex flex-row border border-white border-opacity-10 p-6 gap-6 flex-wrap">
-        <div className="w-80 h-80 bg-[#141414]">
-          {/* Placeholder for the image or map */}
-        </div>
-        <div className="flex-grow">
-          <div className="flex flex-col gap-4">
-            <div className="text-secondary">Status</div>
-            <div className="ml-2 h-6 badge bg-[#ffdc19] text-primary-content rounded-xl">
-              Pending validation
+    <div className="flex flex-row border border-white border-opacity-10 p-6 gap-6 flex-wrap">
+      {deedData?.propertyDetails && (
+        <>
+          <div className="w-80 h-80 bg-[#141414] flex-grow">
+            {/* Image or Map component should go here */}
+          </div>
+          <div className="flex flex-col gap-6 mt-4 flex-grow justify-between">
+            <div className="flex flex-row">
+              <div className="text-secondary">Status</div>
+              <div className="ml-2 h-6 badge bg-[#ffdc19] text-primary-content rounded-xl">
+                Pending validation
+              </div>
             </div>
             <div className="text-5xl font-['Coolvetica'] font-extra-condensed font-bold uppercase">
               {deedData.propertyDetails.propertyAddress}
             </div>
-            <div className="flex flex-col">
-              <span className="text-[10px] font-normal text-zinc-400 uppercase tracking-widest">Owner</span>
-              <Address address={deedData.owner} size="base" />
+            <div className="flex flex-row gap-8 items-center">
+              <div className="flex flex-col">
+                <div className="text-[10px] font-normal text-zinc-400 uppercase tracking-widest">
+                  Owner
+                </div>
+                <Address address={deedData.owner} size="base" />
+              </div>
+              <div className="flex flex-col">
+                <div className="text-[10px] font-normal text-zinc-400 uppercase tracking-widest">
+                  Validator
+                </div>
+                <Address address="The Deed & Title Co." size="base" />
+              </div>
+              <button
+                className="btn btn-outline w-12 p-3 rounded-md"
+                onClick={handleChatClick}
+              >
+                <ChatBubbleBottomCenterTextIcon className="w-full" />
+              </button>
             </div>
-            <div className="flex flex-col">
-              <span className="text-[10px] font-normal text-zinc-400 uppercase tracking-widest">Validator</span>
-              <Address address="The Deed & Title Co." size="base" />
+            <hr className="border-border"></hr>
+            <div className="flex flex-row w-full items-center justify-between">
+              <div className="flex flex-row">
+                <button
+                  className="btn btn-link no-underline text-secondary"
+                  onClick={handleShare}
+                >
+                  <ShareIcon className="h-4" />
+                  Share
+                </button>
+                <button
+                  className="btn btn-link no-underline text-secondary"
+                  onClick={refresh}
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15.39 9.82125C15.39 13.05 12.6337 15.8062 9.24748 15.8062C5.86123 15.8062 3.10498 13.05 3.10498 9.82125C3.10498 6.51375 5.86123 3.83625 9.24748 3.83625H12.0037"
+                      stroke="#A0A0A0"
+                      strokeWidth="0.7875"
+                    />
+                    <path
+                      d="M8.91016 1.15875L12.6902 3.915L8.91016 6.67125"
+                      stroke="#A0A0A0"
+                      strokeWidth="0.7875"
+                    />
+                  </svg>
+                  Refresh
+                </button>
+              </div>
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn m-1">
+                  <EllipsisHorizontalIcon className="h-6" />
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  <li>
+                    <Link href={`/registration/${deedData.id}`}>
+                      <a>Edit</a>
+                    </Link>
+                  </li>
+                  {isValidator && (
+                    <li>
+                      <a onClick={handleMint} className="link-default">
+                        Mint
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </div>
             </div>
-            <button
-              className="btn btn-outline w-12 p-3 rounded-md"
-              onClick={handleChatClick}
-            >
-              <ChatBubbleBottomCenterTextIcon className="w-full" />
-            </button>
           </div>
-        </div>
-      </div>
-
-      <div className="flex flex-row w-full items-center justify-between">
-        <div className="flex flex-row">
-          <button
-            className="btn btn-link no-underline text-secondary"
-            onClick={handleShare}
-          >
-            <ShareIcon className="h-4" />
-            Share
-          </button>
-          <button
-            className="btn btn-link no-underline text-secondary"
-            onClick={refresh}
-          >
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15.39 9.82125C15.39 13.05 12.6337 15.8062 9.24748 15.8062C5.86123 15.8062 3.10498 13.05 3.10498 9.82125C3.10498 6.51375 5.86123 3.83625 9.24748 3.83625H12.0037"
-                stroke="#A0A0A0"
-                strokeWidth="0.7875"
-              />
-              <path
-                d="M8.91016 1.15875L12.6902 3.915L8.91016 6.67125"
-                stroke="#A0A0A0"
-                strokeWidth="0.7875"
-              />
-            </svg>
-            Refresh
-          </button>
-        </div>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} className="btn m-1">
-            <EllipsisHorizontalIcon className="h-6" />
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link href={`/registration/${deedData.id}`}>
-                <a className="link-default">Edit</a>
-              </Link>
-            </li>
-            {isValidator && (
-              <li>
-                <a onClick={handleMint} className="link-default">Mint</a>
-              </li>
-            )}
-          </ul>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
