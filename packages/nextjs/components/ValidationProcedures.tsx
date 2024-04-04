46b0dddc239361e5cbd69d12c4ce7b2e8f2ed19f
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-04 13:00:22 -0600

    Update ValidationProcedures.tsx

--- packages/nextjs/components/ValidationProcedures.tsx
+++ packages/nextjs/components/ValidationProcedures.tsx
@@ -160,7 +160,7 @@ const ValidationProcedures = ({ deedData, onSave, isDraft, onRefresh }: Props) =
                   description="Complete KYB Procedures"
                   fileLabels={["Agreement"]}
                   button={
-                    <button className="btn btn-primary m-1 border-white border-opacity-10 btn-square rounded-lg w-fit px-2 uppercase">
+                    <button className="btn btn-primary m-1 border-white border-opacity-10 btn-square rounded-lg w-fit px-2 text-[9px] font-normal uppercase tracking-widest">
                       View Agreements
                     </button>
                   }
