7c60c08fba033cb29b90b157e00dbf15cabf4561
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-04 12:41:55 -0600

    Update ValidationProcedures.tsx

--- packages/nextjs/components/ValidationProcedures.tsx
+++ packages/nextjs/components/ValidationProcedures.tsx
@@ -54,7 +54,7 @@ const ValidationProcedures = ({ deedData, onSave, isDraft, onRefresh }: Props) =
       <thead>
         <tr>
           <td colSpan={2} className="border border-white border-opacity-10">
-            <div className="text-[12px] font-normal text-white uppercase tracking-widest flex flex-row items-center gap-2 bg-base-300 p-6">
+            <div className="text-[12px] font-normal text-white uppercase tracking-widest flex flex-row items-center gap-2 bg-base-300 p-7">
               <svg
                 width="16"
                 height="16"
