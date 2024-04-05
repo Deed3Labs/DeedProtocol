636f025173cd6b3b4aaee9ad8d62f829216aed1e
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-05 15:12:24 -0600

    Update ValidationPropertyOverview.tsx

--- packages/nextjs/components/ValidationPropertyOverview.tsx
+++ packages/nextjs/components/ValidationPropertyOverview.tsx
@@ -78,7 +78,7 @@ const PropertyOverview = ({ deedData, isOwner, isValidator, refresh }: Props) =>
               <Address address={deedData.owner} label="Owner" size="base" />
               <Address address="The Deed & Title Co." label="Validator" size="base" />
               <button
-                className="btn btn-sm sm:btn-sm border border-white border-opacity-10 w-8 p-0 rounded-md"
+                className="btn btn-sm sm:btn-sm border border-white border-opacity-10 w-8 p-2 rounded-md"
                 onClick={() => handleChatClick()}
               >
                 <ChatBubbleBottomCenterTextIcon className="w-full" />
