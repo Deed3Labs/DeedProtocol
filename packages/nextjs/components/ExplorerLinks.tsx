6c336db0460432930feb60e0ba6840e3613004b7
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-05 23:00:31 -0600

    Update ExplorerLinks.tsx

--- packages/nextjs/components/ExplorerLinks.tsx
+++ packages/nextjs/components/ExplorerLinks.tsx
@@ -9,26 +9,26 @@ const ExplorerLinks = () => {
     <div className="flex flex-row lg:flex-row flex-nowrap justify-items-start px-1 gap-3 lg:gap-6 text-[9.8vw] sm:text-[54px] mb-8 sm:my-8 font-['Coolvetica'] font-extra-condensed font-bold uppercase">
       <Link
         className={`${
-          (type !== "all" && type) || pathname === "/agent-explorer" ? "opacity-30" : ""
+          (type !== "all" && type) || pathname === "/agent-explorer" ? "opacity-30 sm:opacity-30" : ""
         } link-default`}
         href="/property-explorer?type=all"
       >
         All
       </Link>
       <Link
-        className={`${type !== "sale" ? "opacity-30" : ""} link-default`}
+        className={`${type !== "sale" ? "opacity-30 sm:opacity-30" : ""} link-default`}
         href="/property-explorer?type=sale"
       >
         For Sale
       </Link>
       <Link
-        className={`${type !== "lease" ? "opacity-30" : ""} link-default`}
+        className={`${type !== "lease" ? "opacity-30 sm:opacity-30" : ""} link-default`}
         href="/property-explorer?type=lease"
       >
         For Lease
       </Link>
       <Link
-        className={`${pathname !== "/agent-explorer" ? "opacity-30" : ""} link-default`}
+        className={`${pathname !== "/agent-explorer" ? "opacity-30 sm:opacity-30" : ""} link-default`}
         href="/agent-explorer"
       >
         Agent Directory
