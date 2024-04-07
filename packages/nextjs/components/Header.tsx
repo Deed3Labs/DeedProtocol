2b0369e9738a13afeece10c57eedcea9e81d0a8a
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-06 22:51:02 -0600

    Update Header.tsx

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -114,7 +114,7 @@ export const Header = () => {
             {isDrawerOpen && (
               <ul
                 tabIndex={0}
-                className="menu menu-compact dropdown-content left-0 mt-0 mr-3 p-2 shadow border-y-border bg-base-200 flex flex-col w-screen text-2xl gap-4 sm:gap-3"
+                className="menu menu-compact dropdown-content left-0 mt-0 mr-3 px-3.5 pb-2.5 shadow border-y-border bg-base-200 flex flex-col w-screen text-2xl gap-4 sm:gap-3"
                 onClick={() => {
                   setIsDrawerOpen(false);
                 }}
