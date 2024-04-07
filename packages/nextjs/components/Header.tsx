3b381df0a3ac89096174246c1204815575de3377
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-04-06 23:11:36 -0600

    Update Header.tsx

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -102,7 +102,7 @@ export const Header = () => {
           <div className="lg:hidden dropdown" ref={burgerMenuRef}>
             <label
               tabIndex={0}
-              className={`ml-0 btn btn-ghost ${
+              className={`ml-0 btn btn-base-200 ${
                 isDrawerOpen ? "hover:bg-base-300" : "hover:bg-base-300"
               }`}
               onClick={() => {
@@ -114,7 +114,7 @@ export const Header = () => {
             {isDrawerOpen && (
               <ul
                 tabIndex={0}
-                className="menu menu-compact dropdown-content left-0 mt-0 px-3.5 pb-2.5 border-y border-white border-opacity-10 bg-base-200 flex flex-col w-screen text-2xl gap-4 sm:gap-3"
+                className="menu menu-compact dropdown-content left-0 mt-0 px-3.5 pb-2.5 border-b border-white border-opacity-10 bg-base-200 flex flex-col w-screen text-2xl gap-4 sm:gap-3"
                 onClick={() => {
                   setIsDrawerOpen(false);
                 }}
