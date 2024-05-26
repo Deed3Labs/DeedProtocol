c4c604dbd97078c8c299e695e94501c683a76e2a
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-05-25 22:01:40 -0700

    Update [id].tsx

--- packages/nextjs/pages/overview/[id].tsx
+++ packages/nextjs/pages/overview/[id].tsx
@@ -276,9 +276,9 @@ const Page = ({ router }: WithRouterProps) => {
             {/* Map and Pictures */}
             <div className="flex flex-row gap-2 sm:gap-4">
               {/* Map */}
-              <div className="w-[70%] sm:w-1/2 h-[372px] sm:h-[616px] bg-[#141414] border border-white border-opacity-10">
+              <div className="w-[70%] sm:w-1/2 h-full bg-[#141414] border border-white border-opacity-10">
                 {deedData?.propertyDetails && (
-                  <div className="w-full h-full flex-grow">
+                  <div className="w-full h-[372px] sm:h-[616px] flex-grow">
                     <Map
                       markers={[
                         {
@@ -293,7 +293,7 @@ const Page = ({ router }: WithRouterProps) => {
                 )}
               </div>
               {/* Images */}
-              <div className="w-[30%] sm:w-1/2 h-[372px] sm:h-[616px] grid grid-rows-4 sm:grid-cols-2 gap-2 sm:gap-4">
+              <div className="w-[30%] sm:w-1/2 grid grid-rows-4 sm:grid-cols-2 gap-2 sm:gap-4">
                 {pictures?.slice(1, 5).map((picture, index) => (
                   <div key={index} className="w-full h-[85px] sm:h-[300px]">
                     <Image alt="" className="object-cover w-full h-full sm:h-full bg-[#141414] border border-white border-opacity-10" src={picture} />
