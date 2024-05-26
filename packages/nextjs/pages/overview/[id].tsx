a57dde9606b8e57360983afc1a238b0666dea1c8
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-05-25 21:19:11 -0700

    Update [id].tsx

--- packages/nextjs/pages/overview/[id].tsx
+++ packages/nextjs/pages/overview/[id].tsx
@@ -224,7 +224,7 @@ const Page = ({ router }: WithRouterProps) => {
                 )}
               </div>
               {/* Images */}
-              <div className="w-[30%] sm:w-1/2 h-[372px] sm:h-[616px] grid grid-rows-4 sm:grid-cols-2 gap-2 sm:gap-4">
+              <div className="w-[30%] sm:w-1/2 grid grid-rows-4 sm:grid-cols-2 gap-2 sm:gap-4">
                 {pictures?.slice(1, 5).map((picture, index) => (
                   <div key={index} className="w-full h-full">
                     <Image alt="" className="object-cover w-full h-full sm:h-full bg-[#141414] border border-white border-opacity-10" src={picture} />
