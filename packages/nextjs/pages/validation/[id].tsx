3e40e73367fb8aadd2dc98cc58f9cb9a14ec409a
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-06-09 14:39:42 -0700

    Update [id].tsx

--- packages/nextjs/pages/validation/[id].tsx
+++ packages/nextjs/pages/validation/[id].tsx
@@ -208,7 +208,7 @@ const Page = ({ router }: WithRouterProps) => {
             </button>
           </div>
         ) : (
-          <div className="w-full px-4 md:px-8 lg:px-16">
+          <div className="w-full px-1.5 sm:px-3">
             {/* Property Validation Title and Switcher Buttons */}
             <div className="flex flex-row w-full items-center justify-between mb-4 sm:mb-6">
               <div className="hidden sm:flex items-center text-xl sm:text-2xl w-auto">
