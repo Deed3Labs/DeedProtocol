053c58e90d972bee8755d147afe89f4d51844780
Isaiah "KyngKai" Litt <74473762+KyngKai909@users.noreply.github.com>
2024-06-14 18:14:02 -0700

    Update [id].tsx

--- packages/nextjs/pages/overview/[id].tsx
+++ packages/nextjs/pages/overview/[id].tsx
@@ -198,7 +198,7 @@ const Page = ({ router }: WithRouterProps) => {
             </button>
           </div>
         ) : (
-          <div className="w-full px-1.5 sm:px-2">
+          <div className="w-full px-1.5 sm:px-1">
             {/* Property Overview Title and Switcher Buttons */}
             <div className="flex flex-row w-full items-center justify-between mb-4 sm:mb-6">
               <div className="hidden sm:flex items-center text-xl sm:text-2xl w-auto">
